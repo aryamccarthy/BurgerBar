d5e0bd1db1d91e11784e60936fd3316ebf18db7c
lukeoglesbee <lukeoglesbee@usr105-161.perunanet.smu.edu>
2014-10-08 20:09:01 -0500

    slim

--- rest/index.php
+++ rest/index.php
@@ -5,10 +5,14 @@ require 'vendor/autoload.php';
 $app = new \Slim\Slim();
 
 // Hello World function
-$app->get('/hello/:last/:first/:MI', function($last, $first, $MI) {
-	echo "Hello, $first $MI. $last!";
-});
+$app->get('/hello/:last/:first/:MI', 'hello');
+
 
 $app->run();
+
+function hello($last, $first, $MI) {
+	echo "Hello, $first $MI. $last!";
+}
+
 
 ?>
