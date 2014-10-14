7dfac639b19bd8ace0128d371ccbb33d37a80f92
lukeoglesbee <lukewoglesbee@gmail.com>
2014-10-14 12:23:19 -0500

    Updated js for the php version of pages.

--- js/include.js
+++ js/include.js
@@ -1,11 +1,26 @@
-$(document).ready(fillCommonTags);
+$(document).ready(function() {
+	//TODO: Delete fillCommonTags(). See note below. -Luke
+	fillCommonTags():
 
+	var ticket = document.getElementById('active_ticket');
+}
+
+//TODO: start using the php files and delete this.
+//	Luke put this stuff in header.php
 function fillCommonTags() {
   $("header").first().load("header.html");
   $("nav").first().load("nav.html");
-};
+}
 
 // TODO: fix this to show the contents of payment-from.php
 function launchDialog() { 
     
-};
+}
+
+function updateTicket() {
+    var firstname = document.getElementsByClassName('burger_label');
+    var burgerToAdd=firstname[0].innerHTML;
+    var entry = document.createElement('li');
+    entry.innerHTML=burgerToAdd;
+    ticket.appendChild(entry);                                                          
+ }
