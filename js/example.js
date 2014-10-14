da5b81fd5d3a3a25706ff4eefa25c79c6fd0429a
kaurorah <khabeck@smu.edu>
2014-10-14 11:55:56 -0500

    Can now dynamically add burgers to the ticket. Still need to be able to delete them from ticket and view/calculate prices

--- js/example.js
+++ js/example.js
@@ -1,3 +1,13 @@
 function launchDialog() { 
     document.getElementById("paymentDialog").showModal(); 
 } 
+
+var ticket = document.getElementById('active_ticket');
+
+function updateTicket() {
+    var firstname = document.getElementsByClassName('burger_label');
+    var burgerToAdd=firstname[0].innerHTML;
+    var entry = document.createElement('li');
+    entry.innerHTML=burgerToAdd;
+    ticket.appendChild(entry);                                                          
+ }
