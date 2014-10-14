d7da38c39654dffd84cac4125e161c0e3729aa08
aryamccarthy <arya.mccarthy@gmail.com>
2014-10-14 13:28:17 -0500

    Form protection JS, new about us.

--- js/include.js
+++ js/include.js
@@ -4,3 +4,11 @@ function fillCommonTags() {
   $("header").first().load("header.html");
   $("nav").first().load("nav.html");
 };
+
+/* Protects your partially-filled custom order form. */
+function closeEditorWarning() {
+    return 'It looks like you have been editing something -- if you leave before submitting your changes will be lost.';
+}
+if (document.getElementById('ordering')) {
+    window.onbeforeunload = closeEditorWarning;
+}
