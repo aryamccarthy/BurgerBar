7faaa4885459cea65c775746db08d8a3da7483d8
lukeoglesbee <lukewoglesbee@gmail.com>
2014-10-16 11:26:38 -0500

    order-confirmation.php updated

--- order-confirmation.php
+++ order-confirmation.php
@@ -1,15 +1,15 @@
 <?php
-$pageTitle = "Burger Bar";
-include('/include/header.php');
+$pageTitle = "Burger Bar: Order Confirmation";
+include('include/header.php');
 ?>
 <main>
-	<header>
-		<h1>Thanks for your order!</h1>
-	</header>
-	<article>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	</article>
+    <header>
+        <h1>Thanks for your order!</h1>
+    </header>
+    <article>
+        <p>Your order should be ready within the next 25 minutes. For information, including the Burger Bar food truck's current location, call <a href="tel:+18005552564">800-555-2564</a>. For a decade, the Burger Bar has provided top-notch service to the Dallas-Fort Worth Metroplex, and we're proud to serve every delicious, fresh burger from only the finest Ebola-free fare. We appreciate your patronage and the entirety of DFW's cultural mixtures. Visit us or our sister truck, the Taco Truck, today!</p>
+    </article>
 </main>
 <?php
-include('/include/footer.php');
+include('include/footer.php');
 ?>
